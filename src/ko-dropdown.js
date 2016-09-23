c77356d891cbd4c2b409d8c999551fa8dbe6a845
Matthew <matthewcnitschke@gmail.com>
2016-09-23 14:54:57 -0600

    Added ability to change value programmiclly

--- src/ko-dropdown.js
+++ src/ko-dropdown.js
@@ -1,56 +1,90 @@
-ko.components.register("ko-dropdown", {
-  viewModel: function(params){
-    var self = this;
+(function(){
+  ko.components.register("ko-dropdown", {
+    viewModel: function(params){
+      var self = this;
 
-    self.caption = params.caption;
-    self.value = params.value;
-    self.options = params.options;
+      self.caption = params.caption;
+      self.value = params.value;
+      self.options = params.options;
 
-    self.dropdownOpen = ko.observable();
+      self.dropdownOpen = ko.observable();
 
-    self.selectedText = ko.observable();
-    if (ko.unwrap(self.caption)){
-      self.selectedText(self.caption);
-    }
+      self.selectedOption = ko.observable();
+      self.selectedText = ko.computed(function() {
+        return ko.unwrap(self.selectedOption) ? ko.unwrap(self.selectedOption).text : "";
+      });
 
-    self.selectOption = function(option){
-      self.value(option.value);
-      self.selectedText(option.text);
-    }
+      if (ko.unwrap(self.caption)){
+        self.selectedOption({
+          text: self.caption
+        });
+      }
 
-    self.dropdownOptions = ko.computed(function(){
-      var retOptions = [];
-      self.options().forEach(function(option){
-        if (typeof option === 'object' || option instanceof Object){
-          retOptions.push(option);
-        } else {
-          retOptions.push({
-            value: option,
-            text: option
-          });
+      // selects an option provided as a parameter (called from both html and component model)
+      self.selectOption = function(option){
+        self.value(option.value);
+        self.selectedOption(option);
+      }
+
+      self.toggleOpen = function(data, event){
+        event.stopPropagation();
+        self.dropdownOpen(!self.dropdownOpen());
+      }
+
+      self.dropdownOptions = ko.computed(function(){
+        var retOptions = [];
+        self.options().forEach(function(option){
+          if (typeof option === 'object' || option instanceof Object){
+            retOptions.push(option);
+          } else {
+            retOptions.push({
+              value: option,
+              text: option
+            });
+          }
+        });
+
+        if (retOptions.length > 0 && !ko.unwrap(self.caption)) {
+          self.selectOption(retOptions[0]);
+        }
+
+        return retOptions;
+      });
+
+      // updates the selected option if value is changed from the viewModel
+      self.value.subscribe(function(newValue){
+        var selectedValue = ko.unwrap(self.selectedOption) ? ko.unwrap(self.selectedOption).value : null;
+
+        if (newValue !== selectedValue){
+          var option = findOptionByValue(newValue);
+          if (option != null){
+            self.selectOption(option);
+          }
         }
       });
 
-      if (retOptions.length > 0 && !ko.unwrap(self.caption)) {
-        self.selectOption(retOptions[0]);
+      window.addEventListener('click', function (e) {
+          self.dropdownOpen(false);
+      });
+
+      function findOptionByValue(val){
+        var foundOption = null;
+        if (ko.unwrap(self.dropdownOptions)){
+          self.dropdownOptions().forEach(function(option){
+            if (option.value === val){
+              foundOption = option;
+            }
+          });
+        }
+        return foundOption;
       }
+    },
+    template: "<div class='dropdown' data-bind='click: toggleOpen'>\
+        <span class='selected' data-bind='text: selectedText'></span>\
+        <ul data-bind='visible: dropdownOpen, foreach: dropdownOptions'>\
+          <li data-bind='text: text, click: $parent.selectOption'></li>\
+        </ul>\
+      </div>"
+  });
 
-      return retOptions;
-    });
-
-    self.toggleOpen = function(data, event){
-      event.stopPropagation();
-      self.dropdownOpen(!self.dropdownOpen());
-    }
-
-    window.addEventListener('click', function (e) {
-        self.dropdownOpen(false);
-    });
-  },
-  template: "<div class='dropdown' data-bind='click: toggleOpen'>\
-      <span class='selected' data-bind='text: selectedText'></span>\
-      <ul data-bind='visible: dropdownOpen, foreach: dropdownOptions'>\
-        <li data-bind='text: text, click: $parent.selectOption'></li>\
-      </ul>\
-    </div>"
-});
+}());
