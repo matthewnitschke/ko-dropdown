4103811d4b4e042f6d46aadadae3fe862a68b685
Matthew Nitschke <mathuc33@gmail.com>
2017-04-27 15:02:25 -0600

    Simple formatting

--- src/ko-dropdown.js
+++ src/ko-dropdown.js
@@ -1,121 +1,120 @@
-(function () {
-    var dropdowns = [];
+(function() {
+	var dropdowns = [];
 
-    var hideAllDropdowns = function () {
-        for (var i = 0; i < dropdowns.length; i++) {
-            dropdowns[i].dropdownOpen(false);
-        }
-    }
+	var hideAllDropdowns = function() {
+		for (var i = 0; i < dropdowns.length; i++) {
+			dropdowns[i].dropdownOpen(false);
+		}
+	}
 
-    window.addEventListener('click', hideAllDropdowns);
+	window.addEventListener('click', hideAllDropdowns);
 
-    ko.components.register("ko-dropdown", {
-        viewModel: function (params) {
-            var self = this;
+	ko.components.register("ko-dropdown", {
+		viewModel: function(params) {
+			var self = this;
 
-            dropdowns.push(self);
+			dropdowns.push(self);
 
-            self.caption = params.caption;
-            self.value = params.value;
-            self.options = params.options;
-            self.isEnabled = params.isEnabled ? params.isEnabled : ko.observable(true); //Enabled by default if isEnabled isn't passed in
+			self.caption = params.caption;
+			self.value = params.value;
+			self.options = params.options;
+			self.isEnabled = params.isEnabled ? params.isEnabled : ko.observable(true); //Enabled by default if isEnabled isn't passed in
 
-      self.dropdownOpen = ko.observable(false);
+			self.dropdownOpen = ko.observable(false);
 
-      self.selectedOption = ko.observable();
-      self.selectedText = ko.computed(function () {
-          return ko.unwrap(self.selectedOption) ? ko.unwrap(self.selectedOption).text : "";
-      });
+			self.selectedOption = ko.observable();
+			self.selectedText = ko.computed(function() {
+				return ko.unwrap(self.selectedOption) ? ko.unwrap(self.selectedOption).text : "";
+			});
 
-      self.selectOption = function (option) {
-          self.value(option.value);
-          self.selectedOption(option);
-      }
+			self.selectOption = function(option) {
+				self.value(option.value);
+				self.selectedOption(option);
+			}
 
-      self.dropdownOptions = ko.computed(function () {
-          var retOptions = [];
-          self.options().forEach(function (option) {
-              if (typeof option === 'object' || option instanceof Object) {
-                  retOptions.push(option);
-              } else {
-                  retOptions.push({
-                      value: option,
-                      text: option
-                  });
-              }
-          });
+			self.dropdownOptions = ko.computed(function() {
+				var retOptions = [];
+				self.options().forEach(function(option) {
+					if (typeof option === 'object' || option instanceof Object) {
+						retOptions.push(option);
+					} else {
+						retOptions.push({
+							value: option,
+							text: option
+						});
+					}
+				});
 
-          if (retOptions.length > 0 && !ko.unwrap(self.caption)) {
-              self.selectOption(retOptions[0]);
-          }
+				if (retOptions.length > 0 && !ko.unwrap(self.caption)) {
+					self.selectOption(retOptions[0]);
+				}
 
-          return retOptions;
-      });
+				return retOptions;
+			});
 
-      self.dropdownOptions.subscribe(function () {
-          self.value.notifySubscribers(self.value()); // so if dropdown options change, update selected value
-      })
+			self.dropdownOptions.subscribe(function() {
+				self.value.notifySubscribers(self.value()); // so if dropdown options change, update selected value
+			})
 
-      if (ko.unwrap(self.value)) {
-          self.selectedOption(findOptionByValue(ko.unwrap(self.value)));
-      } else if (ko.unwrap(self.caption)) {
-          self.selectedOption({
-              text: self.caption
-          });
-      }
-
-
-
-      self.toggleOpen = function (data, event) {
-          event.stopPropagation();
-          self.dropdownOpen(!self.dropdownOpen());
-      }
-
-            self.toggleOpen = function (data, event) {
-                var currentStatus = self.dropdownOpen();
-
-                event.stopPropagation();
-                hideAllDropdowns();
-
-                if (!currentStatus) {
-                    self.dropdownOpen(true);
-                }
-            }
+			if (ko.unwrap(self.value)) {
+				self.selectedOption(findOptionByValue(ko.unwrap(self.value)));
+			} else if (ko.unwrap(self.caption)) {
+				self.selectedOption({
+					text: self.caption
+				});
+			}
 
 
-            self.value.subscribe(function (newValue) {
-                var selectedValue = ko.unwrap(self.selectedOption) ? ko.unwrap(self.selectedOption).value : null;
 
-                if (newValue !== selectedValue) {
-                    var option = findOptionByValue(newValue);
-                    if (option != null) {
-                        self.selectOption(option);
-                    }
-                }
-            });
+			self.toggleOpen = function(data, event) {
+				event.stopPropagation();
+				self.dropdownOpen(!self.dropdownOpen());
+			}
 
-            window.addEventListener('click', function (e) {
-                self.dropdownOpen(false);
-            });
+			self.toggleOpen = function(data, event) {
+				var currentStatus = self.dropdownOpen();
 
-            function findOptionByValue(val) {
-                var foundOption = null;
-                if (ko.unwrap(self.dropdownOptions)) {
-                    self.dropdownOptions().forEach(function (option) {
-                        if (option.value === val) {
-                            foundOption = option;
-                        }
-                    });
-                }
-                return foundOption;
-            }
-        },
-        template: "<div class='ko-dropdown' data-bind='click: toggleOpen, css: {ko_dropdown_disabled: !isEnabled() }'>\
-    <span class='selected' data-bind='text: selectedText'></span>\
-    <!-- ko if: isEnabled --><ul class='ko-dropdown__list' data-bind='visible: dropdownOpen, foreach: dropdownOptions'>\
-        <li class='ko-dropdown__list-item' data-bind='text: text, click: $parent.selectOption'></li>\
-    </ul>\
-    <!-- /ko --></div>"
-    });
+				event.stopPropagation();
+				hideAllDropdowns();
 
+				if (!currentStatus) {
+					self.dropdownOpen(true);
+				}
+			}
+
+
+			self.value.subscribe(function(newValue) {
+				var selectedValue = ko.unwrap(self.selectedOption) ? ko.unwrap(self.selectedOption).value : null;
+
+				if (newValue !== selectedValue) {
+					var option = findOptionByValue(newValue);
+					if (option != null) {
+						self.selectOption(option);
+					}
+				}
+			});
+
+			window.addEventListener('click', function(e) {
+				self.dropdownOpen(false);
+			});
+
+			function findOptionByValue(val) {
+				var foundOption = null;
+				if (ko.unwrap(self.dropdownOptions)) {
+					self.dropdownOptions().forEach(function(option) {
+						if (option.value === val) {
+							foundOption = option;
+						}
+					});
+				}
+				return foundOption;
+			}
+		},
+		template: "<div class='ko-dropdown' data-bind='click: toggleOpen, css: {ko_dropdown_disabled: !isEnabled() }'>\
+              <span class='selected' data-bind='text: selectedText'></span>\
+              <!-- ko if: isEnabled --><ul class='ko-dropdown__list' data-bind='visible: dropdownOpen, foreach: dropdownOptions'>\
+                  <li class='ko-dropdown__list-item' data-bind='text: text, click: $parent.selectOption'></li>\
+              </ul>\
+              <!-- /ko --></div>"
+	});
 }());
